a63be60d536fff53cb585fa9ab991b34fc6b542b
danielpartida <daniel.partida@d-fine.de>
2021-10-08 14:57:10 +0200

    Fix import.

--- 1st_assignment.R
+++ 1st_assignment.R
@@ -1,7 +1,7 @@
-library(igraph)
+# library(igraph)
 
 # For simplicity define platform C as the unsubscribed users
-subscriptions = graph( edges=c("A","A", "B","B", "C","C", "A","B", "A","C", "B","A", "B", "C"))
+#subscriptions = graph( edges=c("A","A", "B","B", "C","C", "A","B", "A","C", "B","A", "B", "C"))
 #subscriptions$weights = c() 
 
 # x_y shows transition from platform x to y
@@ -15,7 +15,7 @@ c_a = 0.2
 c_b = 0.4
 c_c = 1 - c_a - c_b
 
-# Construct matrix A describing the flow trainsition
+## Construct matrix A describing the flow trainsition
 # Each platform is a row. Each column is a different time step
 A = rbind(c(a_a, a_b, a_c), c(b_a, b_b, b_c), c(c_a, c_b, c_c))
 rownames(A) = c( "A","B","C")
@@ -23,22 +23,32 @@ colnames(A) = c("A","B", "C")
 A
 
 ## Now use the Spectram Theorem from class
-e = eigen(A) # Compute eigenvalues & eigenvectors
-ew = e$values # Store eigenvalues of A
+E = eigen(A) # Compute eigenvalues & eigenvectors
+ew = E$values # Store eigenvalues of A
 ew
 
 D = diag(ew) # Create diagonal matrix containing eigenvalues
 D
 
-U = e$vectors # Store eigenvectors of A
+U = E$vectors # Store eigenvectors of A
 U
 
-U_inv = t(U)  # Assumtion taken since U is symmetric
-
+U_inv = solve(U)
 orth = round(U_inv %*% U,digits=0) # Check that eigenvectors are orthogonal
 orth
 
 # Reconstruct matrix A using the eigenvalue decomposition
-B = U %*% D %*% U_inv # Reconstruct matrix A = B
-B
-A
+B = round(U %*% D %*% U_inv,2) # Reconstruct matrix 
+B == A ## Check if our decomposed matrix equals matrix A
+
+## Define starting point u0 (Initial market shares)
+u0 = c(30, 40, 30)
+
+# Define function to compute u_k (transition for kth month)
+Compute_uk = function(k,u0,U,D,U_inv) {
+  return(U %*% (D^k) %*% U_inv %*% u0)
+}
+
+u_2 = Compute_uk(2, u0, U, D, U_inv)
+u_2
+
